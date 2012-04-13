7627a27cfa49d5c4dd1dd01f8561ba668b1f50e6
kgowdhaman <kgowdhaman.deloitte.com>
2012-04-13 15:53:59 +0530

    first commit

---
+++ cloud/main.js
@@ -0,0 +1,15 @@
+var util = require('util');
+/* main.js
+ * All calls here are publicly exposed as REST API endpoints.
+ * - all parameters must be passed in a single JSON paramater.
+ * - the return 'callback' method signature is 'callback (error, data)', where 'data' is a JSON object.
+*/
+
+/* 'getConfig' server side REST API method.
+ * Trivial example of pulling in a shared config file.
+ */
+exports.getConfig = function(params, callback) {
+  console.log("In getConfig() call");
+  var cfg = require("config.js");
+  return callback(null, {config: cfg.config});
+};
