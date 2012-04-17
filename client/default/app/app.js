42d98902b07636f6e1fb8642db789ce7ca488734
kgowdhaman <kgowdhaman.deloitte.com>
2012-04-17 17:43:53 +0530

    updated

--- client/default/app/app.js
+++ client/default/app/app.js
@@ -2,9 +2,26 @@ Ext.application({
     name: 'Sencha',
 
     launch: function() {
-        Ext.create('Ext.Panel', {
-            fullscreen: true,
-            html: 'Hello World'
-        });
+        Ext.create('Ext.Carousel', {
+    fullscreen: true,
+
+    defaults: {
+        styleHtmlContent: true
+    },
+
+    items: [
+        {
+            html : 'Item 1',
+            style: 'background-color: #5E99CC'
+        },
+        {
+            html : 'Item 2',
+            style: 'background-color: #759E60'
+        },
+        {
+            html : 'Item 3'
+        }
+    ]
+});
     }
 });
